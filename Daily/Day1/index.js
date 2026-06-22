9d310d96c7025bdfded916bf4cad0360a9422379
Aniruddh Singh <aniruddh7390922053@gmail.com>
2026-06-22 19:00:37 +0530

    first commit

---
+++ Daily/Day1/index.js
@@ -0,0 +1,51 @@
+console.log("hello world");
+
+// VAR VS CONST VS LET
+
+// var a=45
+// var a=50
+// console.log(a);
+//  let b= 78
+// //  let b=80 can not be redeclared
+//  console.log(b);
+
+//  const c= 69
+// //  c=70 can not be updated
+// // const c=100 can not be redeclared
+//  console.log(c);
+
+ // Arithmetic operators
+ let x=4
+ let y=56
+ let add = x+y
+ let sub = x-y
+ let mul = x*y
+ let div= x/y
+ let mod =x%y
+ console.log(add);
+ console.log(sub);
+ console.log(mul);
+ console.log(div);
+ console.log(mod);
+ 
+
+ let res = x!==y
+ console.log(res);
+
+ //Assingment operator
+ let c=66
+  console.log(c++,c++,++c,++c,c,c++,++c,c,c++);
+
+  //logical operator
+  let a = true;
+let b = false;
+
+console.log(a && b); 
+
+console.log(a || b); 
+
+console.log(!a);     
+console.log(!b);     
+
+//Conditional operator
+(5===5)?'a':'b'
